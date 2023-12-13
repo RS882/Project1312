c706cbd5384468327ed94c41839183c8b1c0fa2f
Ruslan Senkin <rsenkin24@gmail.com>
2023-12-13 21:27:36 +0200

    add Epmloyee class

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee extends Person{
+
+    private double salary;
+
+    public Employee(String firstName, String lastName, double salary) {
+        super(firstName, lastName);
+        this.salary=salary;
+    }
+}
